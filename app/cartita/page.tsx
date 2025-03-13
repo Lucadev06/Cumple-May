5e576398ffc11fe36c5efe6644e77230ac4baa79
Lucadev06 <francosimonazzi@hotmail.com>
2025-03-13 16:59:15 -0300

    commit

--- app/cartita/page.tsx
+++ app/cartita/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { Box, Typography, Paper } from "@mui/material";
+import {  Typography, Paper } from "@mui/material";
 import { styled } from "@mui/system";
 
 // Estilos 🎀
